1ca1cbfbb11a8cab2c380aa241ac956427a1b118
misterjingles <jningalls@gmail.com>
2015-03-24 09:08:15 -0400

    Tic-Tac-Toe??
    Isn't remotely close to working, but it looks decent-ish

--- ttt.js
+++ ttt.js
@@ -4,15 +4,25 @@ var board = document.getElementById("board");
 
 var turn = "x";
 
+var xTiles = [], yTiles = [];
+
 ttt.go = function(space) {
+	
+	var img = document.getElementById(space);
+	
+	img.src = turn + ".png";
+	
 	if (turn === "x") {
 		turn = "o";
 	} else {
 		turn = "x";
 	}
-	
-	var tile = document.getElementById(space);
-	
-	alert("tile.coords=" + tile.coords + "\n" + 
-		  "board.top,left=(" + board.offsetTop + "," + board.offsetLeft + ")");
+}
+
+ttt.reset = function() {
+	for (var i = 1;i < 10;i ++) {
+		var img = document.getElementById("" + i);
+		img.src = "blank.png";
+	}
+	turn = "x";
 }
